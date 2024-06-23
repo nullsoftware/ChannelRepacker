79f47a968a5332ed162188fa126f5fa5c0b88635
nullsoftware <tumyrthebot@gmail.com>
2024-06-23 13:28:49 +0300

    Added version info.

--- ChannelRepacker/App.xaml.cs
+++ ChannelRepacker/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -14,6 +15,8 @@ namespace NullSoftware
     /// </summary>
     public partial class App : Application
     {
+        public Version ApplicationVersion { get; } = Assembly.GetExecutingAssembly().GetName().Version;
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             NNViewRegistrar.RegisterSplat();
